58be654c8138bd2237b75b4c7a0d52bde5d4f422
quek <55364841+quek04@users.noreply.github.com>
2021-09-07 21:05:42 -0500

    port to 1.17.1 nerf can generation chance

--- src/main/java/quek/ibc/IBCMod.java
+++ src/main/java/quek/ibc/IBCMod.java
@@ -1,22 +1,23 @@
 package quek.ibc;
 
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Food;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.loot.*;
-import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.Effects;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.World;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.entries.LootTableReference;
 import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
@@ -33,18 +34,18 @@ public class IBCMod {
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 
     private static final RegistryObject<Item> BEAN_CAN = ITEMS.register("inconspicuous_bean_can", () -> new BeanCanItem((new Item.Properties())
-            .group(ItemGroup.FOOD)
-            .food(new Food.Builder()
-                    .hunger(2)
-                    .saturation(2.4F)
-                    .effect(() -> new EffectInstance(Effects.REGENERATION, 600, 2), 1.0F)
-                    .effect(() -> new EffectInstance(Effects.STRENGTH, 600, 2), 1.0F)
-                    .effect(() -> new EffectInstance(Effects.GLOWING, 600, 0), 1.0F)
-                    .effect(() -> new EffectInstance(Effects.SPEED, 600, 2), 1.0F)
-                    .effect(() -> new EffectInstance(Effects.JUMP_BOOST, 600, 2), 1.0F)
-                    .effect(() -> new EffectInstance(Effects.SLOW_FALLING, 600, 0), 1.0F)
+            .tab(CreativeModeTab.TAB_FOOD)
+            .food(new FoodProperties.Builder()
+                    .nutrition(2)
+                    .saturationMod(2.4F)
+                    .effect(() -> new MobEffectInstance(MobEffects.REGENERATION, 600, 2), 1.0F)
+                    .effect(() -> new MobEffectInstance(MobEffects.DAMAGE_BOOST, 600, 2), 1.0F)
+                    .effect(() -> new MobEffectInstance(MobEffects.GLOWING, 600, 0), 1.0F)
+                    .effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 600, 2), 1.0F)
+                    .effect(() -> new MobEffectInstance(MobEffects.JUMP, 600, 2), 1.0F)
+                    .effect(() -> new MobEffectInstance(MobEffects.SLOW_FALLING, 600, 0), 1.0F)
                     .meat()
-                    .setAlwaysEdible()
+                    .alwaysEat()
                     .build()
             )
     ));
@@ -57,7 +58,7 @@ public class IBCMod {
     @SubscribeEvent
     public static void lootTableLoad(LootTableLoadEvent event) {
         if(event.getName().getPath().contains("chests")) {
-            event.getTable().addPool(LootPool.builder().addEntry(TableLootEntry.builder(new ResourceLocation(MODID, "blocks/ibc_chest")).weight(1)).name("ibc_bean_can").build());
+            event.getTable().addPool(LootPool.lootPool().add(LootTableReference.lootTableReference(new ResourceLocation(MODID, "blocks/ibc_chest")).setWeight(1)).name("ibc_bean_can").build());
         }
     }
 
@@ -67,14 +68,14 @@ public class IBCMod {
         }
 
         @Override
-        public ItemStack onItemUseFinish(ItemStack stack, World worldIn, LivingEntity entityLiving) {
-            super.onItemUseFinish(stack, worldIn, entityLiving);
-            PlayerEntity player = (PlayerEntity) entityLiving;
-            player.dropItem(player.inventory.armorInventory.get(0), true, true);
-            player.dropItem(player.inventory.armorInventory.get(1), true, true);
-            player.dropItem(player.inventory.armorInventory.get(2), true, true);
-            player.dropItem(player.inventory.armorInventory.get(3), true, true);
-            player.inventory.armorInventory.clear();
+        public ItemStack finishUsingItem(ItemStack stack, Level worldIn, LivingEntity entityLiving) {
+            super.finishUsingItem(stack, worldIn, entityLiving);
+            Player player = (Player) entityLiving;
+            player.drop(player.getInventory().armor.get(0), true, true);
+            player.drop(player.getInventory().armor.get(1), true, true);
+            player.drop(player.getInventory().armor.get(2), true, true);
+            player.drop(player.getInventory().armor.get(3), true, true);
+            player.getInventory().armor.clear();
             return stack;
         }
     }
